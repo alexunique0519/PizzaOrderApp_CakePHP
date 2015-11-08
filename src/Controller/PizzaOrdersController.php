4c4fc40fd39de1a094a76f87ed959665adbdd9bc
Alex Yan <alexunique0519@gmail.com>
2015-11-08 15:43:05 -0500

    Modified the edit action, added the complete action

--- src/Controller/PizzaOrdersController.php
+++ src/Controller/PizzaOrdersController.php
@@ -10,7 +10,8 @@ use App\Controller\AppController;
  */
 class PizzaOrdersController extends AppController
 {
-
+    //public $helpers = array('Js' => array('Jquery'));
+    //public $components = array('RequestHandler');
     /**
      * Index method
      *
@@ -32,14 +33,94 @@ class PizzaOrdersController extends AppController
      * @throws \Cake\Network\Exception\NotFoundException When record not found.
      */
     public function view($id = null)
-    {
-        $pizzaOrders = $this->PizzaOrder->get($id, [
-            'contain' => []
-        ]);
-        $this->set('pizzaOrder', $pizzaOrders);
-        $this->set('_serialize', ['pizzaOrder']);
+    {       
+        
+        //$id = 26;
+        $pizzaOrder = $this->PizzaOrders->get($id);
+        
+        
+        
+        $this->set(compact('pizzaOrder'));
+        
     }
-
+    
+    private function calculatePizzaCost($province, $size, $toppingString, $crustType)
+    {
+        $cost = 0;
+        
+        //according to size
+        switch ($size)
+        {
+        case "small":
+            $cost += 5; 
+          break;
+        case "medium":
+            $cost += 10; 
+          break;
+        case "large":
+            $cost += 15; 
+          break;
+        case "X-L":
+            $cost += 20;
+          break;
+        default:
+            break;
+        }
+        
+        
+        
+        $count = count($toppingString);
+        $cost += ($count - 1) * 0.5;
+        /*
+        $pos = strpos($toppingString, ",");
+        
+        if ($pos === false) 
+        {
+            
+        }
+        else
+        {
+            $toppingArray = explode(",", $toppingString);
+            
+            $count = count($toppingArray);
+            
+            $cost += ($count - 1) * 0.5;
+        
+            $test = $cost;
+        }
+        */
+        
+        if($crustType == "Stuffed")
+        {
+            $cost += 2;
+        }
+        
+        $subTotal = $cost;
+        
+        //calculate by province taxes rate
+        switch ($province)
+        {
+        case "Ontario":
+            $cost *= 1.13; 
+          break;
+        case "Manitoba":
+            $cost *= 1.08; 
+          break;
+        case "Quebec":
+            $cost *= 1.05; 
+          break;
+        case "Saskatchewan":
+            $cost += 1.05;
+          break;
+        default:
+            break;
+        }
+        
+        $total = $cost;
+        
+        return array($subTotal, $total);
+    }
+    
     /**
      * Add method
      *
@@ -47,27 +128,46 @@ class PizzaOrdersController extends AppController
      */
     public function add()
     {
+        $message ="";
+        
         $pizzaOrder = $this->PizzaOrders->newEntity();
         if ($this->request->is('post')) {
+       
+            $province = $_POST['province'];
+            $size = $_POST['size'];
+            $toppingString = $_POST['topping'];
+            $crustType = $_POST['crustType'];
+                 
+            $totalArray = $this->calculatePizzaCost($province, $size, $toppingString, $crustType);
             
-            $toppingsList = implode(",", $_POST["topping"]);
             
+            $toppings = implode(",", $toppingString);
+            
+            //echo $toppingsList;
             $pizzaOrder = $this->PizzaOrders->patchEntity($pizzaOrder, $this->request->data);
             
-            //set value for topping property.
-            $pizzaOrder->set('topping', $toppingsList)；
+            //set value for subTotal property.
+            $pizzaOrder->set('subTotal', round($totalArray[0], 2));
+            $pizzaOrder->set('total', round($totalArray[1], 2));
+            $pizzaOrder->set('topping', $toppings);
             
             if ($this->PizzaOrders->save($pizzaOrder)) {
                 $this->Flash->success(__('The pizza order has been saved.'));
-                return $this->redirect(['action' => 'index']);
+                
+                return $this->redirect(['action' => 'view', $pizzaOrder->id]);
+                
             } else {
                 $this->Flash->error(__('The pizza order could not be saved. Please, try again.'));
             }
+            
+            
+        
         }
-        $this->set(compact('pizzaOrder'));
-        $this->set('_serialize', ['pizzaOrder']);
+        //$this->set(compact('pizzaOrder'));
+        $this->set('pizzaOrder', $pizzaOrder);
+      
     }
-
+    
     /**
      * Edit method
      *
@@ -80,9 +180,17 @@ class PizzaOrdersController extends AppController
         $pizzaOrder = $this->PizzaOrders->get($id, [
             'contain' => []
         ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $pizzaOrder = $this->PizzaOrder->patchEntity($pizzaOrder, $this->request->data);
-            if ($this->PizzaOrder->save($pizzaOrder)) {
+        if ($this->request->is(['post', 'put'])) {
+            
+            $toppingArray = $_POST['topping'];
+            $toppingString = implode(",", $toppingArray);
+          
+            
+            $pizzaOrder = $this->PizzaOrders->patchEntity($pizzaOrder, $this->request->data);
+            $pizzaOrder->set('topping', $toppingString);
+            
+            
+            if ($this->PizzaOrders->save($pizzaOrder)) {
                 $this->Flash->success(__('The pizza order has been saved.'));
                 return $this->redirect(['action' => 'index']);
             } else {
@@ -111,4 +219,26 @@ class PizzaOrdersController extends AppController
         }
         return $this->redirect(['action' => 'index']);
     }
+    
+    public function complete($id)
+    {
+         $pizzaOrder = $this->PizzaOrders->get($id, [
+            'contain' => []
+        ]);
+        if ($this->request->is(['post', 'put'])) {        
+            
+            $pizzaOrder = $this->PizzaOrders->patchEntity($pizzaOrder, $this->request->data);
+            $pizzaOrder->set('orderStatus', 1);
+            
+            
+            if ($this->PizzaOrders->save($pizzaOrder)) {
+                $this->Flash->success(__('The pizza order has been completed.'));
+               
+            } else {
+                $this->Flash->error(__('The pizza order could been completed'));
+            }
+        }
+        return $this->redirect(['action' => 'index']);
+    }
+    
 }
