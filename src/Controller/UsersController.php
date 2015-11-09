e852de6c5ba2886bd53d6ce3107cbd2c5fc7c3cb
Alex Yan <alexunique0519@gmail.com>
2015-11-09 04:53:38 -0500

    add username

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -51,6 +51,8 @@ class UsersController extends AppController
             $user = $this->Auth->identify();
             if ($user) {
                 $this->Auth->setUser($user);
+                
+                    $this->request->session()->write("User.name", $user->username);
                 return $this->redirect($this->Auth->redirectUrl());
             }
             $this->Flash->error(__('Invalid username or password, try again'));
